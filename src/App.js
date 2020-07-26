ad4d8a15f6bd78c908c80491528f593ef3dc80e4
mthang1801 <mthang1801@gmail.com>
2020-07-26 20:16:43 +0700

    facebook authenticate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
+import { TiSocialFacebookCircular } from "react-icons/ti/index";
+import axios from "axios";
 function App() {
+  const responseFacebook = (response) => {
+    console.log(response);
+    const { userID, accessToken } = response;
+    console.log(userID, accessToken);
+    axios({
+      url: `https://graph.facebook.com/v7.0/${userID}/?fields=id,name,picture,email&access_token=${accessToken}`,
+      method: "GET",
+    }).then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Via Facebook</h1>
+      <FacebookLogin
+        appId="270673860694881"
+        autoLoad={false}
+        field="name,email,picture"
+        callback={responseFacebook}
+        icon={<TiSocialFacebookCircular />}
+      />
     </div>
   );
 }
